b2fcc0796c4322fb70a1d2b4862546f7dfd97d12
Juan Barrantes <barrantesjuan513@gmail.com>
2022-02-08 19:53:47 -0500

    correccion ruta API, usando AXIOS -.-

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -1,4 +1,4 @@
-import getData from '@utils/getData.js';
+import getData from '../utils/getData';
 import github from '@images/github.png'
 import twitter from '@images/twitter.png'
 import instagram from '@images/instagram.png'
@@ -6,24 +6,25 @@ import instagram from '@images/instagram.png'
 const Template = async () => {
   const data = await getData();
   console.log({data});
+  const {picture:{large},name:{first,last},email,location:{country}} = data
   const view = `
     <div class="About">
       <div class="card">
         <div class="card_details">
           <div class="card_photo center circle">
-            <img src="${data.picture.large}" alt="${data.name.first}">
+            <img src="${large || '.'}" alt="${first|| '.'}">
             <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="enable-background:new -580 439 577.9 194;"
               xml:space="preserve">
               <circle cx="50" cy="50" r="40" />
             </svg>
           </div>
           <p class="card_title">Hi, My name is</p>
-          <p class="card_value">${data.name.first} ${data.name.last}</p>
+          <p class="card_value">${first|| '.'} ${last|| '.'}</p>
         </div>
         <div class="card_userdata">
           <ul>
-            <li>${data.email}</li>
-            <li>${data.location.country}</li>
+            <li>${email|| '.'}</li>
+            <li>${country|| '.'}</li>
           </ul>
         </div>
         <div class="card_social">
--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,13 +1,14 @@
-const API = process.env.API;
-
+const API = process.env.API || "https://randomuser.me/api/";
+const axios =require ('axios');
 const getData = async (id) => {
   const apiURl = id ? `${API}${id}` : API;
   try {
-    const response = await fetch(apiURl);
-    const data = await response.json();
+    const response = await axios.get(apiURl);
+    const data = await response.data;
     return data.results[0];
   } catch (error) {
     console.log('Fetch Error', error);
+    return []
   };
 };
 
